39c43220603319c7b06728cbd0f29a3e52f8e3ae
Turkish Delight <ilocodes21@gmail.com>
2024-12-20 13:38:54 +0400

    Create var-let.swift

---
+++ day 1/var-let.swift
@@ -0,0 +1,16 @@
+import Cocoa
+
+# Creating Variables
+var greeting = "Hello, playground"
+
+var name = "Ted"
+name = "Rebecca"
+name = "Keeley"
+
+# Creating Constants
+
+let character = "Daphne"
+
+let character = "Daphne"
+character = "Eloise"
+character = "Francesca"
