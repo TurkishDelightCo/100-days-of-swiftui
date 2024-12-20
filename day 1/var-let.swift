cbd1da992631f3cc5f1f5ea9dceeb498f77a7daa
Turkish Delight <ilocodes21@gmail.com>
2024-12-20 13:42:51 +0400

    Update var-let.swift

--- day 1/var-let.swift
+++ day 1/var-let.swift
@@ -1,16 +1,23 @@
 import Cocoa
 
-# Creating Variables
+//: Creating Variables
 var greeting = "Hello, playground"
 
 var name = "Ted"
 name = "Rebecca"
 name = "Keeley"
 
-# Creating Constants
+//: Creating Constants
 
 let character = "Daphne"
 
 let character = "Daphne"
-character = "Eloise"
-character = "Francesca"
+character = "Eloise" //: Will give error message
+
+var playerName = "Roy"
+print(playerName) //: Prints output the value
+
+//: Convention - Camel Case Example
+let managerName = "Michael Scott"
+let dogBreed = "Samoyed"
+let meaningOfLife = "How many roads must a man walk down?"
