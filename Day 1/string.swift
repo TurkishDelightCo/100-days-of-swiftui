d0f6fb930b0070e815877e636bb16244dfa6f907
Turkish Delight <ilocodes21@gmail.com>
2024-12-21 16:12:26 +0400

    Create string.swift

---
+++ Day 1/string.swift
@@ -0,0 +1,24 @@
+import Cocoa
+
+let actor = "Denzel Washington" 
+
+let filename = "paris.jpg"
+let result = "⭐️ You win! ⭐️"
+
+let quote = "Then he tapped a sign saying \"Believe\" and walked away."
+
+let movie = "A day in
+the life of an
+Apple engineer"
+
+let movie = """
+A day in
+the life of an 
+Apple engineer
+""" // error
+
+print(actor.count) // counts variable 
+
+print(movie.hasPrefix("A day"))
+
+print(filename.hasSuffix(".jpg"))
