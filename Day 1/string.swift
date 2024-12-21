23775467e0b0a6323eeba1001237179f82a6c641
Turkish Delight <ilocodes21@gmail.com>
2024-12-21 20:26:49 +0400

    Update string.swift

--- Day 1/string.swift
+++ Day 1/string.swift
@@ -5,20 +5,22 @@ let actor = "Denzel Washington"
 let filename = "paris.jpg"
 let result = "⭐️ You win! ⭐️"
 
-let quote = "Then he tapped a sign saying \"Believe\" and walked away."
+let quote = "Then he tapped a sign saying \"Believe\" and walked away." 
 
 let movie = "A day in
 the life of an
-Apple engineer"
+Apple engineer" // error
 
 let movie = """
 A day in
 the life of an 
 Apple engineer
-""" // error
+""" // correct
 
 print(actor.count) // counts variable 
 
-print(movie.hasPrefix("A day"))
+print(movie.hasPrefix("A day")) // if its in the movie constant it will return as true
 
 print(filename.hasSuffix(".jpg"))
+print(filename.hasSuffix(".JPG")) // displays as false because Swift is case sensitive
+
