6e55952e3246339e7754f343452b202de156f59e
Turkish Delight <ilocodes21@gmail.com>
2025-01-12 20:56:23 +0400

    Create enum.swift

---
+++ Day 3/enum.swift
@@ -0,0 +1,26 @@
+import Cocoa
+
+var selected = "Monday"
+selected = "Tuesday"
+selected = "January" // problem: not weekday
+selected = "Friday "
+
+enum Weekday {   //enum example
+    case monday
+    case tuesday
+    case wednesday
+    case thursday
+    case friday
+}
+
+var day = Weekday.monday
+day = Weekday.tuesday
+day = Weekday.friday
+
+enum Weekday {  
+    case monday, tuesday, wednesday, thursday, friday  
+}
+
+var day = Weekday.monday
+day = .tuesday
+day = .friday // Swift knows that .friday must refer to Weekday.friday
